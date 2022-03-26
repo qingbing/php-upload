6bc7c2ccae3be4f2a6f31628a6803a1fb56a278e
qingbing <780042175@qq.com>
2022-03-26 13:49:57 +0800

    init

---
+++ src/base/BaseUpload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
+namespace Zf\PhpUpload\base;
+
+
+abstract class BaseUpload
+{
+
+}
---
+++ src/base/IUpload.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
+namespace Zf\PhpUpload\base;
+
+/**
+ * 接口: 文件上传接口
+ *
+ * Interface IUpload
+ * @package Zf\PhpUpload\base
+ */
+interface IUpload
+{
+
+}
